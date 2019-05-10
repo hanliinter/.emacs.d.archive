62f565a7fe1f7285a197a73e7229399b5968bc67
Wang. Hanli <h.wang@seeburger.de>
2019-05-10 17:58:12 +0800

    disable SLIME for windows

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 (add-to-list 'load-path "~/.emacs.d/customize")
 ;;
 (load "common.el")
@@ -11,18 +12,18 @@
 ;;(require 'xcscope)
 
 
-(add-to-list 'load-path "~/.emacs.d/el-get/yasnippet")
-(require 'yasnippet)
-(yas-global-mode 1)
-(yas/load-directory "~/.emacs.d/el-get/yasnippet/snippets")
+;(add-to-list 'load-path "~/.emacs.d/el-get/yasnippet")
+;(require 'yasnippet)
+;(yas-global-mode 1)
+;(yas/load-directory "~/.emacs.d/el-get/yasnippet/snippets")
 
 ;;do not set Tab to yasnippet because use ac-complete,so the yasnippet is used indirectly
 
-(add-to-list 'load-path "~/.emacs.d/el-get/expand-region")
+;(add-to-list 'load-path "~/.emacs.d/el-get/expand-region")
 ;(load "customize/cedet.el")
 (load "er.el")
-(require 'expand-region)
-(global-set-key (kbd "C-,") 'er/expand-region)
+;(require 'expand-region)
+;(global-set-key (kbd "C-,") 'er/expand-region)
 
 ;(load "customize/ack.el")
 (load "ace.el")
@@ -33,10 +34,10 @@
 ;(ido-mode t)
 ;(load "smex.el")
 
-(setq inferior-lisp-program "/usr/local/bin/sbcl") ;
-(add-to-list 'load-path "~/.emacs.d/slime/")
-(require 'slime-autoloads)
-(slime-setup '(slime-scratch slime-editing-commands))
+;(setq inferior-lisp-program "/usr/local/bin/sbcl") ;
+;(add-to-list 'load-path "~/.emacs.d/slime/")
+;(require 'slime-autoloads)
+;(slime-setup '(slime-scratch slime-editing-commands))
 
 ;(require 'lsp) add
 (require 'package)
